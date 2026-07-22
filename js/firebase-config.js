54626cfefcbc410ba1e88a3a8034ff7746058352
mkt918 <nagoyann1818@gmail.com>
2026-07-22 21:03:50 +0900

    Firebase設定値を反映

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,10 +2,10 @@
 // このファイルはpublicなクライアント設定であり、公開リポジトリに含めても問題ありません
 // (Firestoreのセキュリティルールでアクセス制御を行います)。
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: 'AIzaSyCKwVHNOTNzzKmGbBJwkcJCZ0MafkIDgq0',
+  authDomain: 'animal-shogi-4e740.firebaseapp.com',
+  projectId: 'animal-shogi-4e740',
+  storageBucket: 'animal-shogi-4e740.firebasestorage.app',
+  messagingSenderId: '340162767803',
+  appId: '1:340162767803:web:6d6ec193853ce84d34329e',
 };
